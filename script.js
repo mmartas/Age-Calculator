a06f5743b566235d0a7e504d13df566db504bc08
Martin Šupler <martin.supler@seznam.cz>
2025-03-07 16:58:12 +0100

    další chyba opravička

--- script.js
+++ script.js
@@ -18,60 +18,64 @@ let actuallDay = actuallFullDate.getDate();
 myForm.addEventListener("submit", function(event){
     // ZABRÁNĚNÍ REFRESHOVÁNÍ STRÁNKY
     event.preventDefault()
+
+    if([4, 6, 9, 11].includes(Number(inputMonth.value)) && Number(inputDay.value > 30)) {
+        alert("Zadejte prosím platné datum.")
+    } else if ([2].includes(Number(inputMonth.value)) && Number(inputDay.value > 29)) {
+        alert("Zadejte prosím platné datum.")
+    } else {
+        // ODSTRAŇOVÁNÍ ČÁREK(PRÁZDNÝCH HODNOT)
+        if(document.getElementById("dateEmptySpace")) {
+            let noneValueDay = document.querySelector("#dateEmptySpace")
+            noneValueDay.remove()
+        }
+        
+        if(document.getElementById("monthEmptySpace")) {
+            let noneValueMonth = document.querySelector("#monthEmptySpace")
+            noneValueMonth.remove()
+        }
+        
+        if(document.getElementById("yearEmptySpace")) {
+            let noneValueYear = document.querySelector("#yearEmptySpace")
+            noneValueYear.remove()
+        }
+       
+        // VÝPOČET ZBÝVAJÍCH LET, MĚSÍCŮ A DNŮ
+        let searchedYear = actuallYear - Number(inputYear.value)
+        let searchedMonth = actuallMonth - Number(inputMonth.value)
+        let searchedDay = actuallDay - Number(inputDay.value)
     
-    // ODSTRAŇOVÁNÍ ČÁREK(PRÁZDNÝCH HODNOT)
-    if(document.getElementById("dateEmptySpace")) {
-        let noneValueDay = document.querySelector("#dateEmptySpace")
-        noneValueDay.remove()
-    }
+        // DNY
+        if(searchedDay < 0) {
+            let lastMonth = new Date(actuallYear, actuallMonth - 1, 0)
+            searchedDay += lastMonth.getDate();
+            searchedMonth--;
+        }
     
-    if(document.getElementById("monthEmptySpace")) {
-        let noneValueMonth = document.querySelector("#monthEmptySpace")
-        noneValueMonth.remove()
-    }
+        // MĚSÍCE
+        if(searchedMonth < 0) {
+            searchedMonth += 12;
+            searchedYear--;
+        }
     
-    if(document.getElementById("yearEmptySpace")) {
-        let noneValueYear = document.querySelector("#yearEmptySpace")
-        noneValueYear.remove()
-    }
-   
-    // VÝPOČET ZBÝVAJÍCH LET, MĚSÍCŮ A DNŮ
-    let searchedYear = actuallYear - Number(inputYear.value)
-    let searchedMonth = actuallMonth - Number(inputMonth.value)
-    let searchedDay = actuallDay - Number(inputDay.value)
-
-    // DNY
-    if(searchedDay < 0) {
-        let lastMonth = new Date(actuallYear, actuallMonth - 1, 0)
-        searchedDay += lastMonth.getDate();
-        searchedMonth--;
-    }
-
-    // MĚSÍCE
-    if(searchedMonth < 0) {
-        searchedMonth += 12;
-        searchedYear--;
-    }
-
-    // ROKY
-    if(Number(inputMonth.value) > actuallMonth || (Number(inputMonth.value) === actuallMonth && Number(inputDay.value) > actuallDay)) {
-        searchedYear--;
-    }
-
+        // ROKY
+        if(Number(inputMonth.value) > actuallMonth || (Number(inputMonth.value) === actuallMonth && Number(inputDay.value) > actuallDay)) {
+            searchedYear--;
+        }
     
+        // NAPLNĚNÍ NOVÉHO ODSTAVCE HODNOTAMI
+        newDayValue.textContent = searchedDay
+        newMonthValue.textContent = searchedMonth
+        newYearValue.textContent = searchedYear
     
-    // NAPLNĚNÍ NOVÉHO ODSTAVCE HODNOTAMI
-    newDayValue.textContent = searchedDay
-    newMonthValue.textContent = searchedMonth
-    newYearValue.textContent = searchedYear
-
-    // VYPSÁNÍ NOVÝCH HODNOT VE STRÁNCE
-    let newDayValuePlace = document.querySelector("#placeDate").appendChild(newDayValue);
-    let newMonthValuePlace = document.querySelector("#placeMonth").appendChild(newMonthValue);
-    let newYearValuePlace = document.querySelector("#placeYear").appendChild(newYearValue)
-
-    // SMAZÁNÍ ČÍSEL Z FORMULÁŘE
-    inputDay.value = ""
-    inputMonth.value = ""
-    inputYear.value = ""
+        // VYPSÁNÍ NOVÝCH HODNOT VE STRÁNCE
+        let newDayValuePlace = document.querySelector("#placeDate").appendChild(newDayValue);
+        let newMonthValuePlace = document.querySelector("#placeMonth").appendChild(newMonthValue);
+        let newYearValuePlace = document.querySelector("#placeYear").appendChild(newYearValue)
+    
+        // SMAZÁNÍ ČÍSEL Z FORMULÁŘE
+        inputDay.value = ""
+        inputMonth.value = ""
+        inputYear.value = ""
+    }
 })
